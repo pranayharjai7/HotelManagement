f5929e28d3f1b69b53bafc960d0e1a7cde167bb2
Pranay Harjai <pranayharjai7@gmail.com>
2022-06-04 19:07:01 +0200

    Started the database.

--- src/main/java/com/pranayharjai/hotelmanagement/Main.java
+++ src/main/java/com/pranayharjai/hotelmanagement/Main.java
@@ -1,7 +1,31 @@
 package com.pranayharjai.hotelmanagement;
 
+import org.h2.tools.Server;
+import java.sql.SQLException;
 public class Main {
-    public static void main(String[] args) {
+
+    private static final Server server = new Server();
+    public static void main(String[] args) throws SQLException {
+
+        startDatabase();
+
         System.out.println("HOTEL MANAGEMENT");
+
+        stopDatabase();
+    }
+
+    /**
+     * Starting the H2 database using runtool.
+     * @throws SQLException: Database Exception
+     */
+    public static void startDatabase() throws SQLException {
+        server.runTool("-tcp", "-web", "-ifNotExists");
+    }
+
+    /**
+     * Shutting down the H2 Database.
+     */
+    private static void stopDatabase() {
+        server.shutdown();
     }
 }
