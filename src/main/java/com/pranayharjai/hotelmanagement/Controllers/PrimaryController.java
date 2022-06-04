e1c3539ec8c2ccac1e89168f929c38cb73249109
Pranay Harjai <pranayharjai7@gmail.com>
2022-06-04 22:07:58 +0200

    JavaFX-sdk-18.0.1, Primary.fxml added and implemented

---
+++ src/main/java/com/pranayharjai/hotelmanagement/Controllers/PrimaryController.java
@@ -0,0 +1,8 @@
+package com.pranayharjai.hotelmanagement.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class PrimaryController {
+    public void startButtonClicked(ActionEvent actionEvent) {
+    }
+}
